dd481922c68fa5def1481fd7b0999dea1fd371a9
rbonazzola <rodbonazzola@gmail.com>
2022-12-17 14:25:58 +0000

    Fixes to shiny's server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,7 +1,8 @@
 get_best_phenotype_per_locus <- function(locus) {
-  z_var <- gwas_across_runs_df %>% filter(region == locus) %>% top_n(n=1, wt=-P) %>% .$pheno %>% strsplit(., split = "_") %>% .[[1]] %>% .[3]
-  run_id <- gwas_across_runs_df %>% filter(region == locus) %>% top_n(n=1, wt=-P) %>% .$run
-  exp_id <- gwas_across_runs_df %>% filter(region == locus) %>% top_n(n=1, wt=-P) %>% .$pheno %>% strsplit(., split = "_") %>% .[[1]] %>% .[1]
+  best_locus_row_df <- gwas_across_runs_df %>% filter(region == locus) %>% top_n(n=1, wt=-P)
+  exp_id <- best_locus_row_df %>% .$pheno %>% strsplit(., split = "_") %>% .[[1]] %>% .[1]
+  run_id <- best_locus_row_df %>% .$run
+  z_var <-  best_locus_row_df %>% .$pheno %>% strsplit(., split = "_") %>% .[[1]] %>% .[3]
   c(experiment=exp_id, run=run_id, z_var=z_var)
 }
 
@@ -14,7 +15,6 @@ get_manhattan_path <- function(phenotype) {
   z_var <- phenotype['z_var']
   
   filename <-glue::glue("GWAS__{z_var}__{exp_id}_{run_id}__manhattan.png")
-  print(filename)
   glue::glue("{mlruns_dir}/{exp_id}/{run_id}/artifacts/GWAS_adj_10PCs/figures/{filename}")
   
 }
